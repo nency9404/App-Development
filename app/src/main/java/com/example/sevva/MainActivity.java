d38ba24921733cbcf211e09dc7170c78d3c601eb
Priyanshi <kuku20patel@gmail.com>
2024-09-21 14:30:21 +0530

    edit_1

--- app/src/main/java/com/example/sevva/MainActivity.java
+++ app/src/main/java/com/example/sevva/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.sevva;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.Reader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    ArrayList<String> datasource;
+    LinearLayoutManager linearLayoutManager;
+    MyRvAdapter myRvAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        recyclerView = findViewById(R.id.horizontal_img);
+
+    }
+    class MyRvAdapter extends RecyclerView.Adapter<MyRvAdapter.MyHolder>
+    {
+        ArrayList<String> data;
+        public MyRvAdapter(ArrayList<String> data){
+            this.data = data;
+        }
+        @NonNull
+        @Override
+        public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+        }
+
+        class MyHolder extends RecyclerView.ViewHolder
+        {
+            public MyHolder(@NonNull View itemView)
+            {
+                super(itemView);
+            }
+        }
     }
 }
