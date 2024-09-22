6a8934a35d0257cc0fb341638f6e52337da31109
Patel Nency Pradipkumar <nencyppatel9404@gmail.com>
2024-09-22 21:09:52 +0530

    done changes

--- app/src/main/java/com/example/sevva/MainActivity.java
+++ app/src/main/java/com/example/sevva/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sevva;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +15,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText etName, etAddress, etPhone, etEmail, etMessage;
     private Button btnSendMessage;
     private Database dbHandler;
+    private Object view;
 
-     @SuppressLint("MissingInflatedId")
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
